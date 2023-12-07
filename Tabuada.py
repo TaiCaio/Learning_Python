04123a8554db1a57c9d2be87d59b39c3a4ed183d
TaiCaio <caiobmeyer@gmail.com>
2023-12-07 17:30:41 -0300

    Tabuada

---
+++ Tabuada.py
@@ -0,0 +1,4 @@
+n = int(input('Digite um numero inteiro para mostrarmos a sua tabuada: '))
+print('-'*12)
+print('{} x {} = {}\n{} x {} = {}\n{} x {} = {}\n{} x {} = {}\n{} x {} = {}\n{} x {} = {}\n{} x {} = {}\n{} x {} = {}\n{} x {} = {}\n{} x {} = {}'.format(n,1,n*1,n,2,n*2,n,3,n*3,n,4,n*4,n,5,n*5,n,6,n*6,n,7,n*7,n,8,n*8,n,9,n*9,n,10,n*10))
+print('-'*12)
