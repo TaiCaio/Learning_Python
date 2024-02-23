2ae7b0ec60e20da19ad8d000fb730ef045b56c21
Caio B. Meyer <148395936+TaiCaio@users.noreply.github.com>
2024-02-23 10:40:05 -0300

    Update Antecessor, sucessor.py

--- Antecessor, sucessor.py
+++ Antecessor, sucessor.py
@@ -1,2 +1,2 @@
 n = int(input('Digite um número inteiro e diremos seu sucessor e seu antecessor: '))
-print('O antecessor do numéro {} é: {} \n O sucessor é: {}.'.format((n-1),n,(n+1)))
+print('O antecessor do numéro {} é: {} \n O sucessor é: {}.'.format(n,(n-1),(n+1)))
