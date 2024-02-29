4f3f30f1c514eebc976b15a133f682c09f74e542
TaiCaio <caiobmeyer@gmail.com>
2024-02-28 21:14:39 -0300

    att

--- Conversor de medidas.py
+++ Conversor de medidas.py
@@ -1,3 +1,3 @@
 n = float(input('Digite uma distância em metros: '))
-print('A medida de {}m equivale a:\n {}km\n {}hm\n {}dam\n {}dm\n {}cm\n {}mm'.format(n,n/1000,n/100,n/10,n*10,n*100,n*1000))
+print('A medida de {}m equivale a:\n {}km\n {}hm\n {}dam\n {}dm\n {}cm\n {}mm'.format(n,n/1000,n/100,n/10,n*10,n*100,n*10000))
 
